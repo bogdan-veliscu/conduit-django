8846f1cc1eaf0c967c67e5538f69a88ddcb80683
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2021-11-11 17:12:34 +0200

    added article feed support

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     ArticleViewSet, CommentListCreateAPIView, CommentsDestroyAPIView,
-    ArticlesFavoriteAPIView, TagListAPIView
+    ArticlesFavoriteAPIView, TagListAPIView, ArticlesFeedAPIView
 )
 
 router = DefaultRouter(trailing_slash=False)
@@ -12,6 +12,7 @@ router.register(r'articles', ArticleViewSet)
 
 app_name = 'articles'
 urlpatterns = [
+    path('articles/feed', ArticlesFeedAPIView.as_view()),
     path('', include(router.urls)),
     path(
         'articles/<slug:article_slug>/comments',
